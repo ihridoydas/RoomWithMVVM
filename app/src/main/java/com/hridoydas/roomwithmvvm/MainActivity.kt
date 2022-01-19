eaa47451316d204c118b50d4bc04232ea971abd4
Hridoy Das <>
2022-01-19 15:33:31 +0900

    Properly can Use room  database in mainActivity now

--- app/src/main/java/com/hridoydas/roomwithmvvm/MainActivity.kt
+++ app/src/main/java/com/hridoydas/roomwithmvvm/MainActivity.kt
@@ -2,10 +2,25 @@ package com.hridoydas.roomwithmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val appDatabase:RoomDatabase by lazy {
+
+        Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "to-buy-database"
+        ).build()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
